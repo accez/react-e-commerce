141c150a4acb99924d57b4d8e106054669a6289d
SimonPalmgren <simon.palmgren91@gmail.com>
2019-11-11 22:44:21 +0100

    custom-button + form input component + styling

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './views/homepage/homepage.component'
 import ShopPage from './views/shop/shop.component'
+import SignInOut from './views/sign-in-out/sign-in-out.component'
 import Header from './components/header/header.component'
 import './App.css';
 
@@ -12,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInOut} />
+
       </Switch>
     </div>
   );
---
+++ clothing-store/src/views/sign-in-out/sign-in-out.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in.component'
+import './sign-in-out.styles.scss'
+
+const SignInOut = () =>(
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+)
+
+export default SignInOut
