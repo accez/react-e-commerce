7ea5a358cb0ded8e96c4f9cc27e96618ef126a6e
SimonPalmgren <simon.palmgren91@gmail.com>
2020-01-06 18:39:21 +0100

    added another button to my shop page

--- web-shop/src/components/custom-button/custom-button.component.js
+++ web-shop/src/components/custom-button/custom-button.component.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './custom-button.styles.scss'
 
-const CustomButton = ({ children, isGoogleSignIn, isFaceBookSignIn, isSubmit, ...otherProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, isFaceBookSignIn, isSubmit, inverted, ...otherProps }) => (
   <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} 
   ${isFaceBookSignIn ? 'facebook-sign-in' : ''}
-  ${isSubmit ? 'submit-button' : ''} custom-button`}
+  ${isSubmit ? 'submit-button' : ''} custom-button
+  ${inverted ? 'inverted' : ''}`}
     {...otherProps}>
     {children}
   </button>
