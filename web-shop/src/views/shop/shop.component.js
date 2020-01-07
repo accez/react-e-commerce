e0feb0559cb36e64d5487664b2a8571f40daef35
SimonPalmgren <simon.palmgren91@gmail.com>
2020-01-07 17:14:57 +0100

    added redux persist + now storing store items in redux

--- web-shop/src/views/shop/shop.component.js
+++ web-shop/src/views/shop/shop.component.js
@@ -1,25 +1,20 @@
 import React from 'react'
-import SHOP_DATA from './shop.data.js'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { selectShopCollections } from '../../redux/shop/shop.selectors'
+
 import PreviewCollection from '../../components/preview-collection/preview-collection.component'
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <PreviewCollection key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+)
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-  render() {
-    const {collections} = this.state
-    return (<div className="shop-page">
-      {
-        collections.map(({id, ...otherCollectionProps}) => (
-          <PreviewCollection key={id} {...otherCollectionProps} />
-        ))
-      }
-    </div>)
-  }
-}
+const mapPropToState = createStructuredSelector({
+  collections: selectShopCollections
+})
 
-export default ShopPage
+export default connect(mapPropToState)(ShopPage)
