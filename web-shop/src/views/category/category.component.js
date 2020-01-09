9c4281841aea0f98cdd548643863e7a0cac52069
SimonPalmgren <simon.palmgren91@gmail.com>
2020-01-09 11:58:08 +0100

    category page now displays the correct items + styling

--- web-shop/src/views/category/category.component.js
+++ web-shop/src/views/category/category.component.js
@@ -5,10 +5,15 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import './category.styles.scss'
 
 const CategoryPage = ({ category }) => {
-  console.log(category)
+  const { title, items } = category
   return (
-    <div className='category'>
-      <h2>CategoryPage</h2>
+    <div className='category-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {
+          items.map(item => <CollectionItem key={item.id} item={item} />)
+        }
+      </div>
     </div>
   )
 }
