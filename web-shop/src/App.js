485d18ff4571eb18e62d0def888942ff9a24a527
SimonPalmgren <simon.palmgren91@gmail.com>
2020-01-14 13:30:14 +0100

    made app responsive

--- web-shop/src/App.js
+++ web-shop/src/App.js
@@ -10,7 +10,7 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selector'
 import { createStructuredSelector } from 'reselect'
-import './App.css';
+import './App.scss';
 
 class App extends React.Component {
   unsubscribeFromAuth = null
