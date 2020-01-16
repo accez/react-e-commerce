98794b9e2ed10fffee2ea54a760e6e15712b036a
SimonPalmgren <simon.palmgren91@gmail.com>
2020-01-16 13:40:35 +0100

    changed shoping and directory data

--- web-shop/src/redux/directory/directory.reducer.js
+++ web-shop/src/redux/directory/directory.reducer.js
@@ -8,26 +8,26 @@ const INITIAL_STATE = {
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl: 'https://i.ibb.co/cTKq5Vk/jackets-startpage.png',
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://i.ibb.co/CvzGmHx/sneakers-startpage.png',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: 'https://i.ibb.co/ssNnwph/home-women.png',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://i.ibb.co/WWbSHh5/home-men.png',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
